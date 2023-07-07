ab8857f693fc637f050fda5a7f5526c5fc0cec22
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-07 15:39:00 -0400

    feat: initial concept with single drag example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,102 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { DragEvent } from "react";
+import classes from "./page.module.css";
 
 export default function Home() {
+  function dragCardStart(e: DragEvent<HTMLDivElement>) {
+    const target = e.target as HTMLDivElement;
+    console.log("drag");
+    e.dataTransfer.clearData();
+    e.dataTransfer.setData("text/plain", target.id);
+  }
+
+  function dragEnter(e: DragEvent<HTMLDivElement>) {
+    console.log("enter");
+    e.preventDefault();
+  }
+
+  function dragOver(e: DragEvent<HTMLDivElement>) {
+    console.log("over");
+    e.preventDefault();
+  }
+
+  function dropCard(e: DragEvent<HTMLDivElement>) {
+    e.preventDefault();
+    console.log("drop");
+
+    const id = e.dataTransfer?.getData("text");
+
+    if (id && e.target instanceof HTMLDivElement) {
+      const card = document.getElementById(id);
+
+      if (card) {
+        e.target.appendChild(card);
+      }
+    }
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={classes.main}>
+      <div className={classes.board}>
+        <div className={classes.stack}></div>
+        <div className={classes.stack}></div>
+        <div className={classes.stack}></div>
+        <div className={classes.stack}></div>
+      </div>
+      <div className={classes.board}>
+        <div className={classes.stack} onDragOver={dragOver} onDrop={dropCard}>
+          <div className={classes.card}></div>
+        </div>
+        <div className={classes.stack}>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+        </div>
+        <div className={classes.stack}>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+        </div>
+        <div className={classes.stack}>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+        </div>
+        <div className={classes.stack}>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+        </div>
+        <div className={classes.stack}>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+        </div>
+        <div className={classes.stack}>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div className={classes.card}></div>
+          <div
+            id="drag-card"
+            className={classes.card}
+            draggable="true"
+            onDragStart={dragCardStart}
+          ></div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={classes.board}>
+        <div className={classes.stack}></div>
+        <div className={classes.stack}></div>
       </div>
     </main>
-  )
+  );
 }
