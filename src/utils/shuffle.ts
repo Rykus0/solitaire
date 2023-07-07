84f67c277ac22899c1a95802d528c7806e5103f7
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-07 16:39:58 -0400

    feat: add shuffle utility

---
+++ src/utils/shuffle.ts
@@ -0,0 +1,20 @@
+export default function shuffle(array: any[]) {
+  let shuffled = [...array];
+  let currentIndex = shuffled.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [shuffled[currentIndex], shuffled[randomIndex]] = [
+      shuffled[randomIndex],
+      shuffled[currentIndex],
+    ];
+  }
+
+  return shuffled;
+}
