559a00b359da080432cd8429b78f73a201f923a9
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-08 09:12:22 -0400

    feat: add playing card classes

---
+++ src/utils/PlayingCards.ts
@@ -0,0 +1,70 @@
+import shuffle from "./shuffle";
+
+export class Card {
+  private value: CardValue = CardValue.Ace;
+  private suit: CardSuit = CardSuit.Hearts;
+
+  constructor(value: CardValue, suit: CardSuit) {
+    this.value = value;
+    this.suit = suit;
+  }
+
+  getValue(): CardValue {
+    return this.value;
+  }
+
+  getSuit(): CardSuit {
+    return this.suit;
+  }
+}
+
+export enum CardSuit {
+  Hearts,
+  Clubs,
+  Diamonds,
+  Spades,
+}
+
+export enum CardValue {
+  Ace = 1,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King,
+}
+
+export class Deck {
+  private cards: Card[] = [];
+
+  constructor() {
+    Object.values(CardSuit)
+      .filter((v) => !isNaN(Number(v)))
+      .forEach((suit) => {
+        Object.values(CardValue)
+          .filter((v) => !isNaN(Number(v)))
+          .forEach((value) => {
+            this.cards.push(new Card(value as CardValue, suit as CardSuit));
+          });
+      });
+  }
+
+  shuffle() {
+    this.cards = shuffle(this.cards);
+  }
+
+  deal(count: number = 1) {
+    return this.cards.splice(0, count);
+  }
+
+  count() {
+    return this.cards.length;
+  }
+}
