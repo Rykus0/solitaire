244e48e86c9762ba17b07a38c8eaa945feade968
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-09 21:51:13 -0400

    feat: add face down styles and rules to card

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -8,24 +8,26 @@ type CardProps = {
   onDragStart?: (e: DragEvent<HTMLDivElement>) => void;
   // TODO: face up or down
   //   - aria label only if face up
-  //   - draggable only if face up
 };
-
-// TODO: scale proportinately to container based on width
 
 export default function Card(props: CardProps) {
   const id = useId();
+  const faceUp = props.card.getFaceUp();
+  const suit = props.card.getSuit();
+  const className = faceUp
+    ? classes[CardSuit[suit].toLowerCase()]
+    : classes.back;
 
   return (
     <div
       id={id}
-      className={classes[CardSuit[props.card.getSuit()].toLowerCase()]}
-      draggable="true"
-      onDragStart={props.onDragStart}
-      data-suit={props.card.getSuit()}
+      className={className}
+      draggable={faceUp ? "true" : "false"}
+      onDragStart={faceUp ? props.onDragStart : undefined}
+      data-suit={suit}
       data-value={props.card.getValue()}
     >
-      {props.card.getDisplayValue()}
+      {faceUp && props.card.getDisplayValue()}
     </div>
   );
 }
