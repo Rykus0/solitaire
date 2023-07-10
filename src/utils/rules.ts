7472e890fc427a54c7565a418e2318cf09f8f0af
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-09 23:33:56 -0400

    feat: add rule for stacking on aces

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -10,3 +10,14 @@ export function canStack(card: PlayingCard, targetCard?: PlayingCard) {
 
   return card.getValue() === CardValue.King;
 }
+
+export function canStackOnAce(card: PlayingCard, targetCard?: PlayingCard) {
+  if (targetCard) {
+    return (
+      card.getValue() === targetCard.getValue() + 1 &&
+      card.getSuit() === targetCard.getSuit()
+    );
+  }
+
+  return card.getValue() === CardValue.Ace;
+}
